c27dd15ee56cf2117a36050349044d992917ba60
TimofeyLazarev7391 <tima.lazarev.68@mail.ru>
2021-06-09 21:15:15 +0700

    123

---
+++ 02_Sum.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    double i, o, p;
+    (void) scanf ("%lf %lf", &i, &o);
+    p=i+o;
+    printf ("%lf\n", p);
+    return 0;
+}
+
---
+++ 03_pow.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+double i, p;
+(void)scanf("%lf %lf", &i, &p);
+printf("%lf\n", pow(i, p));
+return 0;
+}
